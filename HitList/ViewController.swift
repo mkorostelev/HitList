ac055ff29adece39e844995fe7582103efa293cb
Maksym Korostelov <Maksym_Korostelov@epam.com>
2019-07-26 23:16:41 +0300

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Maksym Korostelov on 7/26/19.
+//  Copyright © 2019 Maksym Korostelov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
